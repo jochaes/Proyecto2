70029af5999846b015e88e3ee5f31453bb34afa0
Josué Chaves <d4n11083@estudiantec.cr>
2023-10-10 22:13:20 -0600

    Documentacion Interna

--- php/checkStock.php
+++ php/checkStock.php
@@ -1,22 +1,30 @@
+<!-- Check Stock
+
+Este Script verifica si hay suficiente stock para los productos que se van a comprar.
+Se llama antes de cargar la pagina de confirmacion de compra.
+
+-->
+
+
 <?php
-$archivo = '../data/datos.json'; // Nombre del archivo JSON
 
+// Archivo JSON con los datos de los productos
+$archivo = '../data/datos.json'; 
 
-
-// Abre el archivo en modo lectura ('r')
+// Abre el archivo JSON en modo lectura ('r')
 $manejador = fopen($archivo, 'r');
 
-
+// Toma el Json con la lista de productos que viene en el request
 $requestDataItems = file_get_contents( "php://input" ); //$data is now the string '[1,2,3]';
 $requestDataItems = json_decode( $requestDataItems ); //$data is now a php array array(1,2,3)
 
 
-error_log( print_r($requestDataItems, true) );
+//error_log( print_r($requestDataItems, true) );
 
-
+//Si se pudo abrir el archivo
 if ($manejador) 
 {
-    // Lee el contenido del archivo JSON en una variable
+    // Carga el contenido del archivo JSON en una variable
     $json_data = fread($manejador, filesize($archivo));
 
     // Cierra el archivo
@@ -26,13 +34,14 @@ if ($manejador)
     $datos = json_decode($json_data, true); 
 
 
+    // Recorre el array de productos que viene en el request
+    // Busca cada uno en el array de productos del archivo JSON
+    // Si no hay suficiente stock para alguno, devuelve un error
     foreach ($requestDataItems as $item) {
         $id = $item->id;
         $cantidadPedido = $item->cantidad;
 
-        error_log( print_r($id, true) );
-
-
+        //error_log( print_r($id, true) );
 
         $cont=0;
         while ($cont<count($datos)) 
